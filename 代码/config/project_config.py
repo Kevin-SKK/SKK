b77d773766edf16cc7338f861dd45fb67cc4672d
Kevin-SKK <83170485+Kevin-SKK@users.noreply.github.com>
2024-11-11 11:42:14 +0800

    Delete 代码 directory

--- 代码/config/project_config.py
+++
@@ -1,9 +0,0 @@
-# 写我们的整个项目的所有的配置信息
-import time
-
-# ###### json_server相关的配置信息 #####
-log_file_root_path = "/Users/xiechen/Desktop/郑州303/day01/代码/logs"  # 日志路径
-log_name = f'py_etl_{time.strftime("%Y_%m_%d_%H", time.localtime(time.time()))}.log'  # 日志文件名
-json_file_root_path = "/Users/xiechen/Desktop/郑州303/day01/代码/01_josn数据"
-
-
